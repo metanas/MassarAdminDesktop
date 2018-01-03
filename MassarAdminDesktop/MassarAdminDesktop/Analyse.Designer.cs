3e63ae40275f7b045a1752a84daa17b4f752cf3d
yassineaachir123 <yassine.aachir1@gmail.com>
2018-01-03 18:01:17 +0000

    HHHHHHHHHHHoooomeee

--- MassarAdminDesktop/MassarAdminDesktop/Analyse.Designer.cs
+++ MassarAdminDesktop/MassarAdminDesktop/Analyse.Designer.cs
@@ -28,89 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Analyse));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bunifuTileButton2 = new Bunifu.Framework.UI.BunifuTileButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bunifuTileButton1 = new Bunifu.Framework.UI.BunifuTileButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.Betudiant = new Bunifu.Framework.UI.BunifuTileButton();
-            this.Bprofesseur = new Bunifu.Framework.UI.BunifuTileButton();
-            this.Bclasse = new Bunifu.Framework.UI.BunifuTileButton();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.bunifuTileButton2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.bunifuTileButton1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.Betudiant);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(771, 487);
+            this.panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 30.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(104, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(562, 47);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Groupe Scolaire Al Amana ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(559, 438);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 20);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "30";
+            // 
+            // bunifuTileButton2
+            // 
+            this.bunifuTileButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
+            this.bunifuTileButton2.color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
+            this.bunifuTileButton2.colorActive = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
+            this.bunifuTileButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuTileButton2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold);
+            this.bunifuTileButton2.ForeColor = System.Drawing.Color.White;
+            this.bunifuTileButton2.Image = global::MassarAdminDesktop.Properties.Resources._class;
+            this.bunifuTileButton2.ImagePosition = 21;
+            this.bunifuTileButton2.ImageZoom = 50;
+            this.bunifuTileButton2.LabelPosition = 43;
+            this.bunifuTileButton2.LabelText = "Classes";
+            this.bunifuTileButton2.Location = new System.Drawing.Point(504, 317);
+            this.bunifuTileButton2.Margin = new System.Windows.Forms.Padding(6);
+            this.bunifuTileButton2.Name = "bunifuTileButton2";
+            this.bunifuTileButton2.Padding = new System.Windows.Forms.Padding(0, 0, 0, 10);
+            this.bunifuTileButton2.Size = new System.Drawing.Size(141, 147);
+            this.bunifuTileButton2.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(372, 438);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 20);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "30";
+            // 
+            // bunifuTileButton1
+            // 
+            this.bunifuTileButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
+            this.bunifuTileButton1.color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
+            this.bunifuTileButton1.colorActive = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
+            this.bunifuTileButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuTileButton1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold);
+            this.bunifuTileButton1.ForeColor = System.Drawing.Color.White;
+            this.bunifuTileButton1.Image = global::MassarAdminDesktop.Properties.Resources.professeur;
+            this.bunifuTileButton1.ImagePosition = 21;
+            this.bunifuTileButton1.ImageZoom = 50;
+            this.bunifuTileButton1.LabelPosition = 43;
+            this.bunifuTileButton1.LabelText = "Professeurs";
+            this.bunifuTileButton1.Location = new System.Drawing.Point(317, 317);
+            this.bunifuTileButton1.Margin = new System.Windows.Forms.Padding(6);
+            this.bunifuTileButton1.Name = "bunifuTileButton1";
+            this.bunifuTileButton1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 10);
+            this.bunifuTileButton1.Size = new System.Drawing.Size(141, 147);
+            this.bunifuTileButton1.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(182, 438);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "30";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MassarAdminDesktop.Properties.Resources.school;
+            this.pictureBox1.Location = new System.Drawing.Point(231, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(304, 191);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
             // 
             // Betudiant
             // 
             this.Betudiant.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
-            this.Betudiant.color = System.Drawing.Color.SeaGreen;
-            this.Betudiant.colorActive = System.Drawing.Color.MediumSeaGreen;
+            this.Betudiant.color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
+            this.Betudiant.colorActive = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
             this.Betudiant.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Betudiant.Font = new System.Drawing.Font("Century Gothic", 15.75F);
+            this.Betudiant.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold);
             this.Betudiant.ForeColor = System.Drawing.Color.White;
-            this.Betudiant.Image = ((System.Drawing.Image)(resources.GetObject("Betudiant.Image")));
-            this.Betudiant.ImagePosition = 20;
+            this.Betudiant.Image = global::MassarAdminDesktop.Properties.Resources.etudiant;
+            this.Betudiant.ImagePosition = 21;
             this.Betudiant.ImageZoom = 50;
-            this.Betudiant.LabelPosition = 41;
-            this.Betudiant.LabelText = "Tile 1";
-            this.Betudiant.Location = new System.Drawing.Point(67, 247);
+            this.Betudiant.LabelPosition = 43;
+            this.Betudiant.LabelText = "Etudiants";
+            this.Betudiant.Location = new System.Drawing.Point(127, 317);
             this.Betudiant.Margin = new System.Windows.Forms.Padding(6);
             this.Betudiant.Name = "Betudiant";
-            this.Betudiant.Size = new System.Drawing.Size(128, 129);
-            this.Betudiant.TabIndex = 5;
-            // 
-            // Bprofesseur
-            // 
-            this.Bprofesseur.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
-            this.Bprofesseur.color = System.Drawing.Color.SeaGreen;
-            this.Bprofesseur.colorActive = System.Drawing.Color.MediumSeaGreen;
-            this.Bprofesseur.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Bprofesseur.Font = new System.Drawing.Font("Century Gothic", 15.75F);
-            this.Bprofesseur.ForeColor = System.Drawing.Color.White;
-            this.Bprofesseur.Image = ((System.Drawing.Image)(resources.GetObject("Bprofesseur.Image")));
-            this.Bprofesseur.ImagePosition = 20;
-            this.Bprofesseur.ImageZoom = 50;
-            this.Bprofesseur.LabelPosition = 41;
-            this.Bprofesseur.LabelText = "Tile 1";
-            this.Bprofesseur.Location = new System.Drawing.Point(233, 247);
-            this.Bprofesseur.Margin = new System.Windows.Forms.Padding(6);
-            this.Bprofesseur.Name = "Bprofesseur";
-            this.Bprofesseur.Size = new System.Drawing.Size(128, 129);
-            this.Bprofesseur.TabIndex = 6;
-            // 
-            // Bclasse
-            // 
-            this.Bclasse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(115)))), ((int)(((byte)(182)))));
-            this.Bclasse.color = System.Drawing.Color.SeaGreen;
-            this.Bclasse.colorActive = System.Drawing.Color.MediumSeaGreen;
-            this.Bclasse.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Bclasse.Font = new System.Drawing.Font("Century Gothic", 15.75F);
-            this.Bclasse.ForeColor = System.Drawing.Color.White;
-            this.Bclasse.Image = ((System.Drawing.Image)(resources.GetObject("Bclasse.Image")));
-            this.Bclasse.ImagePosition = 20;
-            this.Bclasse.ImageZoom = 50;
-            this.Bclasse.LabelPosition = 41;
-            this.Bclasse.LabelText = "Tile 1";
-            this.Bclasse.Location = new System.Drawing.Point(391, 247);
-            this.Bclasse.Margin = new System.Windows.Forms.Padding(6);
-            this.Bclasse.Name = "Bclasse";
-            this.Bclasse.Size = new System.Drawing.Size(128, 129);
-            this.Bclasse.TabIndex = 7;
+            this.Betudiant.Padding = new System.Windows.Forms.Padding(0, 0, 0, 10);
+            this.Betudiant.Size = new System.Drawing.Size(141, 147);
+            this.Betudiant.TabIndex = 15;
             // 
             // Analyse
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(583, 391);
-            this.Controls.Add(this.Bclasse);
-            this.Controls.Add(this.Bprofesseur);
-            this.Controls.Add(this.Betudiant);
+            this.ClientSize = new System.Drawing.Size(771, 487);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Analyse";
             this.Text = "Analyse";
             this.Load += new System.EventHandler(this.Analyse_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private Bunifu.Framework.UI.BunifuTileButton bunifuTileButton2;
+        private System.Windows.Forms.Label label2;
+        private Bunifu.Framework.UI.BunifuTileButton bunifuTileButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private Bunifu.Framework.UI.BunifuTileButton Betudiant;
-        private Bunifu.Framework.UI.BunifuTileButton Bprofesseur;
-        private Bunifu.Framework.UI.BunifuTileButton Bclasse;
     }
 }
