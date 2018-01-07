5f1b3f004b3ae53f74ebb9cdc446267efe308541
ramziho <nedjari24@gmail.com>
2018-01-07 13:02:03 +0000

    done

--- MassarAdminDesktop/MassarAdminDesktop/viewprofesseur.cs
+++ MassarAdminDesktop/MassarAdminDesktop/viewprofesseur.cs
@@ -19,9 +19,9 @@ namespace MassarAdminDesktop
 
         private void viewprofesseur_Load(object sender, EventArgs e)
         {
-            Login.read = DBConnect.Gets("select nom,prenom from enseignant");
+            Login.read = DBConnect.Gets("select e.nom,e.prenom,avg(ex.note) from annee as a , examiner as ex, groupe as g , enseignant as e , groupe_matiere_enseignant as gme where gme.id_enseignant=e.id  and gme.id_groupe=g.id and gme.id_groupe=ex.id_groupe and gme.id_matiere=ex.id_matiere and ex.id_annee=a.id and a.annee_scolaire='"+Home.idann+"' group by e.id");
             while (Login.read.Read())
-                bunifuCustomDataGrid1.Rows.Add(Login.read["nom"].ToString(), Login.read["prenom"].ToString());
+                bunifuCustomDataGrid1.Rows.Add(Login.read[0].ToString(), Login.read[1].ToString(),(Math.Round(Double.Parse(Login.read[2].ToString())*10,2))+"%");
             Login.read.Close();
         }
         private void Groupe_Resize(object sender, EventArgs e)
